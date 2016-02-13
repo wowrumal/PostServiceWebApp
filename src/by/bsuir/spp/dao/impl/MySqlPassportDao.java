3aa143696ca6ce42c7aa42718d52813d2f9ee27c
Wowrumal <wowrumal@mail.ru>
2016-02-13 18:51:18 +0300

    some dao

--- src/by/bsuir/spp/dao/impl/MySqlPassportDao.java
+++ src/by/bsuir/spp/dao/impl/MySqlPassportDao.java
@@ -2,7 +2,14 @@ package by.bsuir.spp.dao.impl;
 
 import by.bsuir.spp.bean.Passport;
 import by.bsuir.spp.dao.PassportDao;
+import by.bsuir.spp.dao.connectionpool.impl.ConnectionPoolImpl;
 import by.bsuir.spp.exception.dao.DaoException;
+import by.bsuir.spp.exception.dao.connectionpool.ConnectionPoolException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MySqlPassportDao implements PassportDao {
 
@@ -14,9 +21,33 @@ public class MySqlPassportDao implements PassportDao {
         return instance;
     }
 
+    private static final String INSERT_PASSPORT_QUERY = "INSERT into 'passport' (passportNumber, address, issuingInsistution, issueDate) "+
+                                                        "values (?, ?, ?, ?)";
+
     @Override
     public Integer create(Passport newInstance) throws DaoException {
-        return null;
+        int id = 0;
+
+        try(Connection connection = (Connection) ConnectionPoolImpl.getInstance();
+            PreparedStatement statement = connection.prepareStatement(INSERT_PASSPORT_QUERY, java.sql.Statement.RETURN_GENERATED_KEYS))
+        {
+            statement.setString(1, newInstance.getPassportNumber());
+            statement.setString(2, newInstance.getAddress());
+            statement.setString(3, newInstance.getIssuingInsitution());
+            statement.setDate(4, newInstance.getIssueDate());
+
+            try(ResultSet resultSet = statement.getGeneratedKeys())
+            {
+                if(resultSet.next())
+                {
+                    id = resultSet.getInt(1);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return id;
     }
 
     @Override
